a400047dbb47e1f2fdecb53d6197fe2e6b0e2d85
Test User <test@test.com>
2020-07-23 09:13:54 +0000

    Test tidy

--- site-modules/testing/manifests/init.pp
+++ site-modules/testing/manifests/init.pp
@@ -1,6 +1,11 @@
 class testing {
 
-  exec { 'clear ca cert':
-    command => '/usr/bin/rm /etc/puppetlabs/puppet/ssl/certs/ca.pem',
+  if $facts['os']['family'] == 'RedHat' {
+    $certpath = '/etc/puppetlabs/puppet/ssl/certs/ca.pem'
+  } else {
+    $certpath = 'C:\ProgramData\PuppetLabs\puppet\etc\ssl\certs\ca.pem'
+  } 
+  tidy { 'clear ca cert':
+    path => '/usr/bin/rm /etc/puppetlabs/puppet/ssl/certs/ca.pem',
   }
 }

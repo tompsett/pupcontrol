cbf838fa736b4a6ac29dd5bf510ccfc656b5ec40
Test User <test@test.com>
2020-07-23 09:16:14 +0000

    Test tidy

--- site-modules/testing/manifests/init.pp
+++ site-modules/testing/manifests/init.pp
@@ -6,6 +6,6 @@ class testing {
     $certpath = 'C:\ProgramData\PuppetLabs\puppet\etc\ssl\certs\ca.pem'
   } 
   tidy { 'clear ca cert':
-    path => '/usr/bin/rm /etc/puppetlabs/puppet/ssl/certs/ca.pem',
+    path => $certpath,
   }
 }
